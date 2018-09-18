53d5a463b607d250f578d16423989e4291a070cd
Charlie Dill <charliedill@charlies-air.lan>
2018-09-18 13:42:36 -0500

    converto to MVC program

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Charlie Dill on 9/12/18.
+//  Copyright © 2018 dill. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
